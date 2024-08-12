55b7cd0a2b413308407843629da1da3d67fa4ca1
ChulHee Kim <solroya@gmail.com>
2024-08-12 17:50:33 +0900

    cors 모듈 설치 및 설정

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ const __filename = fileURLToPath(import.meta.url);
 import express from "express";
 import mariadb from "mariadb"
 import dotenv from "dotenv";
+import cors from "cors"
+
 dotenv.config();
 
 const app = express()
 
+app.use(cors());
 app.use(express.json()); //json 포맷 인식
 // route : .get(): 받기, .post(): 보내기, .put(): 보내서 부분 수정, .delete() : 보내서 삭제
 // RESTful API : REpresentational (대표성 있는 방식으로 요청 URL을 생성하는 규칙)
@@ -36,10 +39,6 @@ app.get('/getAllUsers', function (req, res) {
           res.status(200).json(rows);
           return conn.end();  // res 응답객체에서 제공하는 .json()메소드로 데이터를 전송
           // return conn.end(); // 또다른 요청에 응답하기 위해 한번 요청처리하면 접속 끊기
-        })
-        .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-          conn.end();
         })
         .catch(err => {
           //handle error
