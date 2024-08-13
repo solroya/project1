240fee0c4c13946f6b41dc85c7bbc84d4e266fef
ChulHee Kim <solroya@gmail.com>
2024-08-13 15:53:28 +0900

    userContoroller 분기

--- server/contoller/userController.js
+++ server/contoller/userController.js
@@ -6,6 +6,16 @@ const getAllUsers = async (req, res) => {
         res.status(200).json(users);
     } catch (err) {
         console.log(err);
-        res.status(500);
+        res.status(500).json({
+            status : "db connection is failed",
+            message: "데이터베이스 연결이 실패했습니다."
+        })
     }
 }
+
+//Object(객체) : 여러가지 변수, 값, 함수, 클래스 ... 포함 할 수 있는 자바스크립트 데이터 타입
+const userController = {
+    getAllUsers
+}
+
+export default userController;
