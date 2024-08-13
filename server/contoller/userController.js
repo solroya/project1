845c3e0475c7b97bfb5d1be82b09e16dab0ae2fa
ChulHee Kim <solroya@gmail.com>
2024-08-13 15:48:40 +0900

    userController 분기

--- server/contoller/userController.js
+++ server/contoller/userController.js
@@ -1,5 +1,11 @@
 import userModel from "../models/userModel.js";
 
 const getAllUsers = async (req, res) => {
-    
+    try {
+        const users = await userModel.getAllUsers(); //사용자 정보를 모두 조회해서 users 변수에 담음
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+    }
 }
